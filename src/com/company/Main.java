4ba5785b771bca19f9fa7b28f6c25f2dfaa7b512
ochernetskiy <chernetskiyoleg09@gmail.com>
2020-08-09 12:24:40 +0400

    Basic Calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    //constructor
+    public Main() {}
+
+    //add
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    //subtract
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //multiply
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //divide
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Main myCalculator = new Main();
+        System.out.println(myCalculator.add(81, 62));
+        System.out.println(myCalculator.subtract(812, 369));
+        System.out.println(myCalculator.multiply(12, 24));
+        System.out.println(myCalculator.divide(125, 5));
+        System.out.println(myCalculator.multiply(358, 752));
+    }
+}
