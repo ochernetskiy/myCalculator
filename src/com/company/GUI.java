03290fe81637c22a7bd11c3ec1210f2bc4cf782b
ochernetskiy <chernetskiyoleg09@gmail.com>
2020-08-11 16:30:31 +0400

    Basic Calculator

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -7,10 +7,19 @@ import javax.swing.*;
 public class GUI {
 
     static GraphicsConfiguration gc;
-    public static void main(String[] args) {
+    public GUI() {
         JFrame frame = new JFrame(gc);
         frame.setTitle("My First Application");
         frame.setSize(350, 500);
+        frame.setLayout(null);
         frame.setVisible(true);
+
+        JButton button = new JButton("Button");
+        frame.add(button);
+        button.setBounds(0,0,50,50);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
     }
 }
