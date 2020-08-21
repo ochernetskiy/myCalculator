b1258b0445d5c9909c7e5c8506ad88a412da8db4
ochernetskiy <chernetskiyoleg09@gmail.com>
2020-08-21 15:32:06 +0400

    Basic Calculator

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -15,21 +15,27 @@ public class GUI extends JFrame {
         JPanel grid = new JPanel();
         grid.setLayout(new GridLayout(4, 3));
 
-        for (int b = 0; b < 10; b++) {
+        for (int b = 1; b < 10; b++) {
             grid.add(new JButton("" + b));
-            if(b == 2) {
+            if(b == 3) {
                 JButton add = new JButton("+");
                 grid.add(add);
             }
-            if(b == 5) {
+            if(b == 6) {
                 JButton subtract = new JButton("-");
                 grid.add(subtract);
             }
-            if(b == 8) {
+            if(b == 9) {
+
                 JButton multiply = new JButton("*");
                 grid.add(multiply);
-            }
-            if(b == 9) {
+
+                JButton clear = new JButton("C");
+                grid.add(clear);
+
+                JButton zero = new JButton("0");
+                grid.add(zero);
+
                 JButton divide = new JButton("/");
                 grid.add(divide);
 
